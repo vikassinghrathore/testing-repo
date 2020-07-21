d7a91abeb568e438b1d9fd75d3fd5d9356fa7aae
vikas <vikaskanvika@gmail.com>
2020-07-21 21:51:57 +0530

    first commit

---
+++ springmvc-first/src/main/java/webapp/LoginService.java
@@ -0,0 +1,9 @@
+package webapp;
+
+	public class LoginService {
+		public boolean validateUser(String user, String password) {
+			return user.equalsIgnoreCase("in28Minutes") && password.equals("dummy");
+		}
+
+	}
+
